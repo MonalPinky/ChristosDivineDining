970026d79b69b152b0c2b17da1fc815cc53f1f17
MonalPinky <109946990+MonalPinky@users.noreply.github.com>
2023-09-21 14:30:05 +0200

    dynamic text align
    Signed-off-by: MonalPinky <109946990+MonalPinky@users.noreply.github.com>

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-
+  textalign = 'left';
+  constructor() {
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      // true for mobile device
+      this.textalign = 'center';
+    }
+  }
 }
